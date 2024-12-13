964680898749f05e1479ef7a8496ee25cafdc974
Shea <s.coughlin037@mybvc.ca>
2024-12-12 21:09:02 -0700

    Made Gravity gun function

--- Assets/Scripts/Targeter.cs
+++ Assets/Scripts/Targeter.cs
@@ -4,35 +4,111 @@
 public class Targeter : MonoBehaviour
 {
     [SerializeField] private Transform targetOrigin;
+    [SerializeField] private Camera cam;
     [SerializeField] private float targetDistance = 1000f;
     [SerializeField] private LayerMask hitLayer;
     private AimTarget _currentTarget;
+    private Rigidbody grabbedRB;
+
+    [Header("Gravity Gun Settings")]
+    [SerializeField] private float maxGrabDistance = 10f;
+    [SerializeField] private float lerpSpeed = 10f;
+    [SerializeField] private float throwForce = 20.0f;
+    [SerializeField] private Transform objectHolder;
     void Update()
     {
-        Vector3 origin = transform.position;
-        RaycastHit hit;
-        if (Physics.Raycast(origin, targetOrigin.transform.forward, out hit, targetDistance, hitLayer))
-        {
-            if (hit.collider.gameObject.TryGetComponent<AimTarget>(out AimTarget target))
-            {
-                SwapTarget(target);
-                return;
-            }
-        }
-        _currentTarget?.StopTarget();
-        _currentTarget = null;
+
+        HandleTargeting();
+        HandleGrabInput();
+        HandleObjectMovement();
+
 
     }
 
-    private void SwapTarget(AimTarget target)
+    private void UpdateTarget(AimTarget target)
     {
-        if (target != _currentTarget)
+        if (target == _currentTarget) return;
         {
             _currentTarget?.StopTarget();
             _currentTarget = target;
             _currentTarget.Target();
         }
     }
-    
-    
+
+
+    private void HandleTargeting()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        if (Physics.Raycast(ray, out RaycastHit hit, targetDistance, hitLayer))
+        {
+            if (hit.collider.TryGetComponent<AimTarget>(out AimTarget target))
+            {
+                UpdateTarget(target);
+                return;
+            }
+        }
+    }
+    private void HandleObjectMovement()
+    {
+        if (grabbedRB)
+        {
+            grabbedRB.MovePosition(Vector3.Lerp(grabbedRB.position, objectHolder.position, Time.deltaTime * lerpSpeed));
+
+            if (Input.GetMouseButtonDown(0)) 
+            {
+                ThrowObject();
+            }
+        }
+    }
+
+    private void ClearCurrentTarget()
+    {
+        _currentTarget?.StopTarget();
+        _currentTarget = null;
+    }
+
+    private void HandleGrabInput()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (grabbedRB)
+            {
+                ReleaseObject();
+            }
+            else
+            {
+                TryGrabObject();
+            }
+        }
+    }
+
+
+    private void TryGrabObject()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        if (Physics.Raycast(ray, out RaycastHit hit, maxGrabDistance, hitLayer))
+        {
+            if (hit.collider.TryGetComponent<Rigidbody>(out Rigidbody rb))
+            {
+                grabbedRB = rb;
+                grabbedRB.isKinematic = true;
+            }
+        }
+    }
+
+    private void ReleaseObject()
+    {
+        if (grabbedRB)
+        {
+            grabbedRB.isKinematic = false;
+            grabbedRB = null;
+        }
+    }
+
+    private void ThrowObject()
+    {
+        grabbedRB.isKinematic = false;
+        grabbedRB.AddForce(cam.transform.forward * throwForce, ForceMode.VelocityChange);
+        grabbedRB = null;
+    }
 }
